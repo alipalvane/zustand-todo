bd5038101449110362aa3c51bb0f39bca1af3f00
Ali <alipalvane@gmail.com>
2023-11-17 13:25:12 +0330

    create form to create todo list

--- src/components/columns.tsx
+++ src/components/columns.tsx
@@ -3,17 +3,14 @@ import { useModalStore } from "@/lib/store";
 import Column from "./column";
 import Modal from "./modal";
 export default function Columns() {
-  const {closeModal,isOpen,openModal} = useModalStore()
-
-  console.log(isOpen)
-
+  const {isOpen,openModal} = useModalStore()
   return (
     <div>
       <button
         onClick={openModal}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
-        OPEN THY MODAL
+        + Add New To Do
       </button>
       <section className="mt-10 flex gap-6 lg:gap-12">
         <Column title="Todo" status="TODO" />
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,9 +1,23 @@
 "use client";
 
-import { useModalStore } from "@/lib/store";
+import { useModalStore, useTaskStore } from "@/lib/store";
 
 export default function Modal() {
-  const closeModal = useModalStore(state=>state.closeModal)
+  const closeModal = useModalStore((state) => state.closeModal);
+  const addTask = useTaskStore((state) => state.addTask);
+
+  const handleSumbit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const { title, description } = Object.fromEntries(formData);
+
+    if (typeof title !== "string" || typeof description !== "string") return;
+
+    addTask(title, description);
+    closeModal();
+  };
   return (
     <div
       className="fixed z-10 inset-0 overflow-y-auto"
@@ -23,36 +37,57 @@ export default function Modal() {
         >
           &#8203;
         </span>
-        <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+        <form
+          onSubmit={handleSumbit}
+          className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
+        >
           <div className="sm:flex sm:items-start">
-            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <div className="my-3 text-center sm:mt-0 sm:text-left">
               <h3
                 className="text-lg leading-6 font-medium text-gray-900"
                 id="modal-title"
               >
-                Basic Modal
+                Add New To Do
               </h3>
               <div className="mt-2">
-                <p className="text-sm text-gray-500">Message dito ng kahit anong pwede ilagay na kunyari info ganun </p>
+                <p className="text-sm text-gray-500">
+                  Add your to do task here
+                </p>
               </div>
             </div>
           </div>
+          <div>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Todo Title"
+              className="w-full border border-1 rounded-md p-2 text-sm mb-2 text-gray-900"
+            />
+            <textarea
+              id="description"
+              name="description"
+              cols={30}
+              rows={3}
+              placeholder="description"
+              className="w-full border border-1 rounded-md p-2 text-sm text-gray-900"
+            />
+          </div>
           <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
             <button
-              type="button"
-              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-            >
-              OK
-            </button>
-            <button
-      
               onClick={closeModal}
               className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
             >
               Close
             </button>
+            <button
+              type="submit"
+              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+            >
+              Add
+            </button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
